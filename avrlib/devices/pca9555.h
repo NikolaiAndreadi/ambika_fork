21375ea292ec8af7a0b9f665ab5415c24be3bba5
Nikolai Andreadi <nikolayandreadi@gmail.com>
2022-01-16 22:42:57 +0300

    Owner header, reformat file

--- avrlib/devices/pca9555.h
+++ avrlib/devices/pca9555.h
@@ -1,6 +1,8 @@
 #ifndef _PCA9555_H
 #define _PCA9555_H
 
+// PCA9555_input class by https://github.com/NikolaiAndreadi
+
 #include <avrlib/i2c/i2c.h>
 #include <avrlib/base.h>
 
@@ -8,38 +10,37 @@ namespace avrlib {
 
 template <typename InterruptPin, uint8_t user_addr>
 class PCA9555_input {
-  private:
-    Word data;
-    static const uint8_t hw_addr = 0x40;
-    static constexpr uint8_t addr = hw_addr + (user_addr << 1);
-  public:
-    PCA9555_input() { 
-      InterruptPin::set_mode(DIGITAL_INPUT); 
-    }
+ private:
+  Word data;
+  static const uint8_t hw_addr = 0x40;
+  static constexpr uint8_t addr = hw_addr + (user_addr << 1);
+ public:
+  PCA9555_input() { 
+    InterruptPin::set_mode(DIGITAL_INPUT); 
+  }
 
-    void Init() {
-      // invert reg values (using pullups)
-      i2c.Begin(addr + I2C_WRITE);
-      i2c.Write(4);
-      i2c.Write(0xff);
-      i2c.Write(0xff);
-      i2c.End();
-    }
+  void Init() {
+    i2c.Begin(addr + I2C_WRITE);
+    i2c.Write(4); // invert register values
+    i2c.Write(0xff); // for 0th register
+    i2c.Write(0xff); // for 1st register
+    i2c.End();
+  }
 
-    void Update () {
-      if (InterruptPin::is_high()) 
-        return;
-      i2c.Begin(addr + I2C_WRITE);
-      i2c.Write(0); // read from zeroth register
-      i2c.Begin(addr +  I2C_READ);
-      data.bytes[0] = i2c.Read(true); // read zeroth reg
-      data.bytes[1] = i2c.Read(); // read first register
-      i2c.End();
-    }
+  void Update () {
+    if (InterruptPin::is_high()) 
+      return;
+    i2c.Begin(addr + I2C_WRITE);
+    i2c.Write(0); // read data
+    i2c.Begin(addr +  I2C_READ);
+    data.bytes[0] = i2c.Read(true); // read zeroth reg
+    data.bytes[1] = i2c.Read(); // read first register
+    i2c.End();
+  }
 
-    Word GetData() { return data; }
+  Word GetData() { return data; }
 
-    DISALLOW_COPY_AND_ASSIGN(PCA9555_input);
+  DISALLOW_COPY_AND_ASSIGN(PCA9555_input);
 };
 
 } // namespace avrlib

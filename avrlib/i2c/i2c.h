9de2d3f604e77dc0fe465774cdb91c61a77bfe29
Nikolai Andreadi <nikolayandreadi@gmail.com>
2022-01-16 22:38:41 +0300

    reformat i2c, added owner header

--- avrlib/i2c/i2c.h
+++ avrlib/i2c/i2c.h
@@ -1,5 +1,15 @@
 #ifndef _I2C_H
 #define _I2C_H
+/************************************************************************* 
+* Title:    C include file for the I2C master interface 
+*           (i2cmaster.S or twimaster.c)
+* Author:   Peter Fleury <pfleury@gmx.ch>  http://jump.to/fleury
+* File:     $Id: i2cmaster.h,v 1.10 2005/03/06 22:39:57 Peter Exp $
+* Software: AVR-GCC 3.4.3 / avr-libc 1.2.3
+* Target:   any AVR device
+* Usage:    see Doxygen manual
+**************************************************************************/
+// Library modified by https://github.com/NikolaiAndreadi
 
 #include <avrlib/base.h>
 #include <compat/twi.h>
@@ -11,16 +21,21 @@ namespace avrlib {
 
 template <uint32_t frequency>
 class I2cMaster {
-  public:
+ public:
+    I2cMaster() {
+        TWSR = 0; // no prescaler
+        TWBR = ((F_CPU/frequency)-16)/2;
+    }
+    
     static bool Begin(uint8_t address) {
         // returns true if success
         uint8_t   twst;
 
 	    TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN); // send START condition
-	    while(!(TWCR & (1<<TWINT))); // wait until transmission completed
+        while(!(TWCR & (1<<TWINT))); // wait until transmission completed
 
-	    twst = TW_STATUS & 0xF8; // check Status Register, mask prescaler bits.
-	    if ( (twst != TW_START) && (twst != TW_REP_START)) return false;
+        twst = TW_STATUS & 0xF8; // check Status Register, mask prescaler bits.
+        if ( (twst != TW_START) && (twst != TW_REP_START)) return false;
 
 	    TWDR = address; // send device address
 	    TWCR = (1<<TWINT) | (1<<TWEN);
@@ -34,11 +49,6 @@ class I2cMaster {
     static void End() { 
         TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO); 
         while(TWCR & (1<<TWSTO));
-    }
-
-    I2cMaster() {
-        TWSR = 0; // no prescaler
-        TWBR = ((F_CPU/frequency)-16)/2;
     }
 
     static void Write(uint8_t data) {
